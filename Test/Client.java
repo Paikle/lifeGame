b3b00ed7e975db096fc3690d589dfa837cc92848
e <15084728248@163.com>
2017-12-13 12:50:20 +0800

    Client去乱码
    将乱码去掉

--- Test/Client.java
+++ Test/Client.java
@@ -3,7 +3,7 @@ package Test;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+		
 		DrawTest A = new DrawTest();
 		//new Thread(A).start();
 		//A.paintComponents();
